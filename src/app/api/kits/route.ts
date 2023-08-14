a567ea7894e615ea79ee2ca09f21a027f6c001a4
Patrick Gaskill <patrick.gaskill@gmail.com>
2023-08-14 18:19:30 -0400

    Search working

--- src/app/api/kits/route.ts
+++ src/app/api/kits/route.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import kitsData from "./KITS_SHIPPING_DATA.json";
 
 export async function GET(request: NextRequest) {
-  const labelIdSearchText = request.nextUrl.searchParams.get("labelId");
+  const labelIdSearch = request.nextUrl.searchParams.get("labelId");
 
-  if (!labelIdSearchText) {
+  if (!labelIdSearch) {
     return NextResponse.json([]);
   }
 
-  return NextResponse.json({
-    results: kitsData.filter((kit) => kit.label_id.includes(labelIdSearchText)),
-  });
+  return NextResponse.json(
+    kitsData.filter((kit) => kit.label_id.includes(labelIdSearch))
+  );
 }
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Kit = {
+  id: number;
+  label_id: string;
+  shipping_tracking_code: string;
+};
